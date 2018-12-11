6ca51a25375b4f31b5661b826c3634e13c28a368
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-12-11 08:00:54 -0600

    Added original resolution HB output

--- src/hb_compute.m
+++ src/hb_compute.m
@@ -1,17 +1,23 @@
 function mes = hb_compute(mes)
 
 % Get coef table - use "which" here so matlab will still be able to find
-% this file when we go to compile the code for a spider later
+% this file when we compile the code for a spider
 load(which('all_e_coef.mat'));
 e_coef = all_e_coef;
 
 % Initialize some outputs that we will update one column at a time
 mes.od_tddr_d = [];
+
+mes.hb_oxy = [];
+mes.hb_deoxy = [];
+mes.hb_total = [];
+
 mes.hb_oxy_d = [];
 mes.hb_deoxy_d = [];
 mes.hb_total_d = [];
 
-% Go through each channel and compute Hb
+
+%% Go through each channel and compute Hb
 for c = 1:mes.nchannels
 	ind700 = 2*c-1;
 	ind830 = 2*c;
@@ -53,6 +59,9 @@ for c = 1:mes.nchannels
 	
 	% Oxy Hb
 	if ((eoxy_700*edeo_830 - eoxy_830*edeo_700)~=0)
+		mes.hb_oxy(:,c) = ...
+			(mes.od_tddr(:,ind700)*edeo_830 - mes.od_tddr(:,ind830)*edeo_700) ...
+			/ (eoxy_700*edeo_830 - eoxy_830*edeo_700);
 		mes.hb_oxy_d(:,c) = ...
 			(mes.od_tddr_d(:,ind700)*edeo_830 - mes.od_tddr_d(:,ind830)*edeo_700) ...
 			/ (eoxy_700*edeo_830 - eoxy_830*edeo_700);
@@ -60,18 +69,22 @@ for c = 1:mes.nchannels
 	
 	% Deoxy Hb
 	if ((edeo_700*eoxy_830 - edeo_830*eoxy_700)~=0)
+		mes.hb_deoxy(:,c) = ...
+			(mes.od_tddr(:,ind700)*eoxy_830 - mes.od_tddr(:,ind830)*eoxy_700) ...
+			/ (edeo_700*eoxy_830 - edeo_830*eoxy_700);
 		mes.hb_deoxy_d(:,c) = ...
 			(mes.od_tddr_d(:,ind700)*eoxy_830 - mes.od_tddr_d(:,ind830)*eoxy_700) ...
 			/ (edeo_700*eoxy_830 - edeo_830*eoxy_700);
 	end
 	
 	% Total Hb
+	mes.hb_total(:,c) = mes.hb_oxy(:,c) + mes.hb_deoxy(:,c);
 	mes.hb_total_d(:,c) = mes.hb_oxy_d(:,c) + mes.hb_deoxy_d(:,c);
-	
 	
 end
 
-% Now downsample the non-NIRS stuff (event markers etc)
+
+%% Now downsample the non-NIRS stuff (event markers etc)
 prerow = (1:size(mes.od_tddr,1))';
 postrow = downsample(prerow,factor);
 
@@ -95,11 +108,8 @@ for k = 1:length(m)
 	mes.bodymovement_d(possibles(1)) = mes.bodymovement(m(k));
 end
 
-% For the time, identify the time  and put it
-% in the downsampled data at the closest available time.
-
+% For the time, identify the time at each sample.
 mes.time_d = mes.time(postrow);
-
 
 % For the removal mark, identify the time of each removal mark and put it
 % in the downsampled data at the closest available time.
@@ -121,9 +131,10 @@ for k = 1:length(m)
 	mes.prescan_d(possibles(1)) = mes.prescan(m(k));
 end
 
+return
 
-% Function to lookup coefficient from a table.  If not exist, do
-% interpolation
+
+%% Function to lookup coefficient from a table.  If not exist, interpolate
 function e = find_e(waveLength, oxydeoxy, table)
 
 if(strcmp(oxydeoxy, 'oxy'))
@@ -137,8 +148,10 @@ end
 l = floor(waveLength);
 u = floor(waveLength) + 1;
 
-lc = table(find(table(:,1)==l),col);
-uc = table(find(table(:,1)==u),col);
+lc = table(table(:,1)==l,col);
+uc = table(table(:,1)==u,col);
 
 e = (uc - lc) / (u - l) * (waveLength - l) + lc;
+
 return;
+
