52030487d95dfb346580e629a1afa88af88c5fd4
Baxter Rogers <baxter.rogers@vanderbilt.edu>
2018-12-12 13:55:15 -0600

    Added xvfb and imagemagick

--- test_sing_container.sh
+++ test_sing_container.sh
@@ -2,10 +2,10 @@
 
 singularity run \
 --cleanenv \
---home INPUTS \
---bind INPUTS:/INPUTS \
---bind OUTPUTS:/OUTPUTS \
-baxpr-nirsqa.simg \
+--home /wkdir/INPUTS \
+--bind /wkdir/INPUTS:/INPUTS \
+--bind /wkdir/OUTPUTS:/OUTPUTS \
+baxpr-nirsqa-master-v1.0.0.simg \
 mes_file /INPUTS/data_MES_Probe1.csv \
 downsample 10 \
 hpf_cutoff_sec 200 \
